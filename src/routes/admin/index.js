3674a2d66933a804d126509f6b52632c5ad8597c
Shubhanil <shubhanil@digitalaptech.co.in>
2024-03-13 20:12:43 +0530

    Company CRUD

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -1,4 +1,25 @@
-const router = require('express').Router()
-const adminControler = require('../../controller/admin/index')
-router.get('/', adminControler.homePage)
-module.exports = router
+const router = require('express').Router();
+
+const adminControler = require('../../controller/admin/index');
+const companyController = require('../../controller/admin/company.controller');
+
+const verifyToken = require('../middleware/authMiddleware');
+
+
+// get all Company
+router.get('/api/companies', companyController.getAllCompanies);
+
+// Company Store
+router.post('/api/company/store', companyController.createCompany);
+
+// View Company
+router.get('/api/company/:id', companyController.getCompanyById);
+
+// Company Delete
+router.delete('/api/company/delete/:id', companyController.deleteCompany);
+
+// Admin dashboard
+router.get('/api/admin-dashboard', adminControler.homePage);
+
+
+module.exports = router;
