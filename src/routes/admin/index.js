b456051aef4a11c5906c3e91c13c93c8b0882ebb
Shubhanil <shubhanil@digitalaptech.co.in>
2024-03-20 20:04:24 +0530

    Company CRUD

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -17,10 +17,13 @@ router.post('/company/store', uploadImg.single('logo'), verifyToken, companyCont
 // router.post('/company/store', uploads.array('logo', 4), verifyToken, companyController.createCompany);
 
 // View Company
-router.get('/company/:id', companyController.getCompanyById);
+router.get('/company/:id', verifyToken, companyController.getCompanyById);
+
+// PUT /api/company/update/:id - Update Company by ID
+router.put('/company/update/:id', uploadImg.single('logo'), verifyToken, companyController.updateCompanyById);
 
 // Company Delete
-router.delete('/company/delete/:id', companyController.deleteCompany);
+router.delete('/company/delete/:id', verifyToken, companyController.deleteCompany);
 
 // Admin dashboard
 router.get('/admin-dashboard', adminControler.homePage);
